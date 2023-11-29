6e2f8f182a3a0b1c4031623d6c8f674a3a3a7e1c
Jeremias Esperanza <142370600+JerryEsperanza@users.noreply.github.com>
2023-11-29 10:40:44 +0800

    Add files via upload

---
+++ UserApp.java
@@ -0,0 +1,22 @@
+package singletonPattern;
+
+public class UserApp {
+    public static void main(String[] args){
+        //instantiate the app
+        Application app = Application.getInstance();
+
+        //login a user
+        app.loginUser("Juan");
+
+        //login another user
+        app.loginUser("Maria");
+
+        //logout present user
+        app.logoutUser();
+
+        //login another user
+        app.loginUser("Maria");
+
+
+    }
+}
